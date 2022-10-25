81f278d67170dd9272dd9912cb0e86c3cabd90ee
Samantha Siao <=>
2022-10-25 14:35:52 +0800

    changed config app

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Nexmo\Laravel\NexmoServiceProvider::class,
+        // Nexmo\Laravel\NexmoServiceProvider::class,
 
     ],
 
