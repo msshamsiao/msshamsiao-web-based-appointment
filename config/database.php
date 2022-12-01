a2501787860b55da772830049cea789483778949
Samantha Siao <=>
2022-12-01 18:31:36 +0800

    change charset

--- config/database.php
+++ config/database.php
@@ -52,7 +52,7 @@ return [
             'username' => env('DB_USERNAME', 'sql6582185'),
             'password' => env('DB_PASSWORD', 'SmEMa8z6wc'),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
+            'charset' => 'utf8',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
